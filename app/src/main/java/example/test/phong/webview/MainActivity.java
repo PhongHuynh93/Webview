1886429aed18d2626907044929ddc8accfdcec64
Phong Huỳnh <p7157568@gmail.com>
2017-07-29 22:30:46 +0700

    feat - add custome in app web browser

--- app/src/main/java/example/test/phong/webview/MainActivity.java
+++ app/src/main/java/example/test/phong/webview/MainActivity.java
@@ -1,11 +1,18 @@
 package example.test.phong.webview;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private ProgressBar progressBar;
     private ImageView imgHeader;
+    private float m_downX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +42,102 @@ public class MainActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         imgHeader = (ImageView) findViewById(R.id.backdrop);
 
+        if (!TextUtils.isEmpty(getIntent().getStringExtra("postUrl"))) {
+            postUrl = getIntent().getStringExtra("postUrl");
+        }
+
+        initWebView();
         // initializing toolbar
         initCollapsingToolbar();
+        renderPost();
+//
+//        // info - enable / disable javascript
+//        webView.getSettings().setJavaScriptEnabled(true);
+////        webView.loadUrl(postUrl);
+//        // info - Loading local html file into web view
+//        webView.loadUrl("file:///android_asset/sample.html");
+//        webView.setHorizontalScrollBarEnabled(false);
+//
+//        /**
+//         * info - Enabling zoom-in controls
+//         * */
+//        webView.getSettings().setSupportZoom(true);
+//        webView.getSettings().setBuiltInZoomControls(true);
+//        webView.getSettings().setDisplayZoomControls(true);
+    }
 
-        // info - enable / disable javascript
+    private void renderPost() {
+        webView.loadUrl(postUrl);
+    }
+
+    // info -  In shouldOverrideUrlLoading() we detect whether the clicked link is internal or external. If it’s external, we launch the BrowserActivity.
+    private void initWebView() {
+        webView.setWebChromeClient(new MyWebChromeClient(this));
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                /**
+                 * Check for the url, if the url is from same domain
+                 * open the url in the same activity as new intent
+                 * else pass the url to browser activity
+                 * */
+                if (Utils.isSameDomain(postUrl, url)) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    intent.putExtra("postUrl", url);
+                    startActivity(intent);
+                } else {
+                    // launch in-app browser i.e BrowserActivity
+                    openInAppBrowser(url);
+                }
+
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        webView.clearCache(true);
+        webView.clearHistory();
         webView.getSettings().setJavaScriptEnabled(true);
-//        webView.loadUrl(postUrl);
-        // info - Loading local html file into web view
-        webView.loadUrl("file:///android_asset/sample.html");
         webView.setHorizontalScrollBarEnabled(false);
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
 
-        /**
-         * info - Enabling zoom-in controls
-         * */
-        webView.getSettings().setSupportZoom(true);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setDisplayZoomControls(true);
+                if (event.getPointerCount() > 1) {
+                    //Multi touch detected
+                    return true;
+                }
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        // save the x
+                        m_downX = event.getX();
+                    }
+                    break;
+
+                    case MotionEvent.ACTION_MOVE:
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP: {
+                        // set x so that it doesn't move
+                        event.setLocation(m_downX, event.getY());
+                    }
+                    break;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void openInAppBrowser(String url) {
+        Intent intent = new Intent(MainActivity.this, BrowserActivity.class);
+        intent.putExtra("url", url);
+        startActivity(intent);
     }
 
     /**
@@ -90,5 +178,15 @@ public class MainActivity extends AppCompatActivity {
                 .crossFade()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(imgHeader);
+    }
+
+    // info -  Here we use a custom MyWebChromeClient() to intercept the methods of WebView.
+    private class MyWebChromeClient extends WebChromeClient {
+        Context context;
+
+        public MyWebChromeClient(Context context) {
+            super();
+            this.context = context;
+        }
     }
 }
