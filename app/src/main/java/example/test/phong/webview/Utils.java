279621fcc4ff5dfb5be605f2b74993222af3d568
Phong Huỳnh <p7157568@gmail.com>
2017-07-29 21:42:47 +0700

    feat -  add back, next, bookmark icon

---
+++ app/src/main/java/example/test/phong/webview/Utils.java
@@ -0,0 +1,69 @@
+package example.test.phong.webview;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.view.MenuItem;
+
+/**
+ * Created by user on 7/29/2017.
+ *  Create a class named Utils.java and add the below code. Here
+
+ > isSameDomain() checks whether an url is from same domain or not. This method will be useful to launch the browser activity incase of external url.
+
+ > bookmarkUrl() method adds or removes an url from bookmarks list using SharedPreferences. isBookmarked() checks whether an url is bookmarked or not.
+
+ > tintMenuIcon() changes the color of toolbar icon. This method is to change the bookmark icon color when an url is bookmarked.
+ */
+
+public class Utils {
+    public static boolean isSameDomain(String url, String url1) {
+        return getRootDomainUrl(url.toLowerCase()).equals(getRootDomainUrl(url1.toLowerCase()));
+    }
+
+    private static String getRootDomainUrl(String url) {
+        String[] domainKeys = url.split("/")[2].split("\\.");
+        int length = domainKeys.length;
+        int dummy = domainKeys[0].equals("www") ? 1 : 0;
+        if (length - dummy == 2)
+            return domainKeys[length - 2] + "." + domainKeys[length - 1];
+        else {
+            if (domainKeys[length - 1].length() == 2) {
+                return domainKeys[length - 3] + "." + domainKeys[length - 2] + "." + domainKeys[length - 1];
+            } else {
+                return domainKeys[length - 2] + "." + domainKeys[length - 1];
+            }
+        }
+    }
+
+    public static void tintMenuIcon(Context context, MenuItem item, int color) {
+        Drawable drawable = item.getIcon();
+        if (drawable != null) {
+            // If we don't mutate the drawable, then all drawable's with this id will have a color
+            // filter applied to it.
+            drawable.mutate();
+            drawable.setColorFilter(ContextCompat.getColor(context, color), PorterDuff.Mode.SRC_ATOP);
+        }
+    }
+
+    public static void bookmarkUrl(Context context, String url) {
+        SharedPreferences pref = context.getSharedPreferences("androidhive", 0); // 0 - for private mode
+        SharedPreferences.Editor editor = pref.edit();
+
+        // if url is already bookmarked, unbookmark it
+        if (pref.getBoolean(url, false)) {
+            editor.putBoolean(url, false);
+        } else {
+            editor.putBoolean(url, true);
+        }
+
+        editor.commit();
+    }
+
+    public static boolean isBookmarked(Context context, String url) {
+        SharedPreferences pref = context.getSharedPreferences("androidhive", 0);
+        return pref.getBoolean(url, false);
+    }
+}
